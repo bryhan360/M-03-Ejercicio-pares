ae14aabdd4307cc5bd53575284d306a7ca143b76
bryhan360 <blaciar.lonsallez@gmail.com>
2017-03-08 00:45:30 +0100

    Add files via upload

---
+++ ejercicio-pares.py
@@ -0,0 +1,8 @@
+# !/usr/bin/python
+# -*-coding: utf-8-*-
+
+numero = input("Introduzca un numero: ")
+if ( numero % 2 == 0 ) :
+	print "Bonito numero "
+else :
+	print "Que numero mas inadecuado xD "
